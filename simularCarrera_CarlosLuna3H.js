02402a40285e58fe0dadaedbd193f55c8e51b852
CharlyLuna <cluna10@ucol.mx>
2021-08-27 17:48:04 -0500

    implemented win condition and message for the winner

--- simularCarrera_CarlosLuna3H.js
+++ simularCarrera_CarlosLuna3H.js
@@ -12,6 +12,9 @@ class Runner {
     return this._position;
   }
   run(amount) {
+    /* probabilidades de avanzar segun el numero del dado:
+      1 o 2 = avanza 1, 3 = avanza 3, 4 5 y 6 = avanza 2;
+    */
     if (amount == 1 || amount == 2) {
       this._position += 1;
     } else if (amount == 3) {
@@ -19,7 +22,6 @@ class Runner {
     } else {
       this._position += 2;
     }
-    return amount;
   }
 }
 let runner1 = new Runner();
@@ -29,12 +31,29 @@ let dice = new Dice();
 let stepsRunner1 = new Array();
 // array en el que se vera cada avance del corredor2;
 let stepsRunner2 = new Array();
+// agregamos la variable ganador para saber si ya hubó un ganador;
+let winner = false;
 
-while (runner1.getPosition() <= 10 || runner2.getPosition() <= 10) {
-  /* solo se esta probando que las posibilidades esten funcionando
-     de acuerdo a lo obtenido en el dado; */
-  console.log(`El dado del corredor1 sacó ${runner1.run(dice.throw())}`);
-  console.log(`El dado del corredor2 sacó ${runner2.run(dice.throw())}`);
+// mientras ninguno llegue a 100 y no haya un ganador sigue el ciclo;
+while (
+  (runner1.getPosition() < 100 || runner2.getPosition() < 100) &&
+  winner === false
+) {
+  //hacemos las tiradas de cada corredor
+  runner1.run(dice.throw());
+  runner2.run(dice.throw());
+  // se establece la condicion de que si uno llega a la cifra establecida es el ganador;
+  if (runner1.getPosition() >= 100 && runner2.getPosition() < 100) {
+    console.log("Ganó el corredor 1");
+    // cambiamos el valor de winner a true si ya hay ganador;
+    winner = true;
+  } else if (runner1.getPosition() >= 100 && runner2.getPosition() >= 100) {
+    console.log("Es un empate");
+    winner = true;
+  } else if (runner1.getPosition() < 100 && runner2.getPosition() >= 100) {
+    console.log("Ganó el corredor 2");
+    winner = true;
+  }
   // pasamos la posicion final de este turno al array de su respectivo corredor;
   stepsRunner1.push(runner1.getPosition());
   stepsRunner2.push(runner2.getPosition());
